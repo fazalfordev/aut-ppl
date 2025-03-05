8fdc7a51de50574ca111c6123568bb5ad1196489
fazalfordev <149255114+fazalfordev@users.noreply.github.com>
2025-03-05 11:00:19 +0700

    Delete aut2025-main directory

--- aut2025-main/nav.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <h1>AUT-NG</h1>
-    <a href="index.php"><i class="fas fa-home"></i>Home</a>
-    <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-    <a href="vpage.php"><i class="fas fa-bug"></i>VPAGE</a>
-</div>
